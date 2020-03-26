db625e62b9ffa7f8c96c1110bd2a4d25cb6a1c31
huseyin <thehusko@gmail.com>
2020-03-26 16:06:20 +0000

    got the js working, updated css

--- main.js
+++ main.js
@@ -1,21 +1,20 @@
-function getImages(){
-    fetch("https://source.unsplash.com/random")
-    .then(response => {
-        console.log(response.url)
-        return response;
-    })
-};
-
-
-async function callImages(){
-    let a = await getImages();
-    let b = await getImages();
+function getImages() {
+    return new Promise(resolve => {
+        setTimeout(() => {
+            resolve(fetch("https://source.unsplash.com/random"))
+        }, 3000);
+    });
+}
+async function getURLs(){
+    const a = await getImages().then(result => result.url);
+    const b = await getImages().then(result => result.url);
+    console.log(a)
     displayImages(a,b)
-};
+}
 
 const displayImages = (x,y) => {
     document.getElementById("image1").innerHTML = `<img src="${x}">`
     document.getElementById("image2").innerHTML = `<img src="${y}">`
-}
+};
 
-callImages()
+getURLs()
